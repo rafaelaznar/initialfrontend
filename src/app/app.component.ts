f06f36927c8524f005a5ab3570e81cd91f9a15e4
Rafael Aznar <rafaaznar@gmail.com>
2024-03-21 08:01:49 +0100

    initial initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,51 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpClientModule, HttpErrorResponse } from '@angular/common/http';
+
+
+export interface initial {
+  id: number;
+  name: string;
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+
+  providers: [HttpClient],
+  imports: [CommonModule, RouterOutlet, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'initialfrontend';
+  title: initial = { id: 0, name: "" };
+
+  constructor(
+    private oHttpClient: HttpClient
+  ) { }
+
+  ngOnInit() {
+    this.get();
+  }
+
+  getOne(id: number): Observable<initial> {
+    return this.oHttpClient.get<initial>("http://localhost:8083/initial/" + id);
+  }
+
+
+  get(): void {
+    this.getOne(1).subscribe({
+      next: (data: initial) => {
+        this.title = data;
+      },
+      error: (error: HttpErrorResponse) => {
+        this.title.name = error.message;
+      }
+
+    })
+
+  }
+
 }
